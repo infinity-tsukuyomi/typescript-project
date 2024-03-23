c0bd456e78af298368d7d61e65db9c800012f24c
Michail Tyupyshev <misha.tyupyshev@gmail.com>
2024-03-23 21:14:12 +0200

    Added genre's page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <nav className={"flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white"}>
+        <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
             <h3>Github Search</h3>
 
             <span>
